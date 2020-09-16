97520c7af8c091175adebbb24241e14dc10a451b
YevgeniyMakarenko <zhekamakarenko532@gmail.com>
2020-09-16 12:06:49 +0300

    final project

--- finaly_project/src/index.js
+++ finaly_project/src/index.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 
 import { Nav } from "./components/header/nav.jsx";
 ReactDOM.render(<Nav />, document.querySelector("#root"));
-=======
-import "./index.scss";
-const test = () => {
-  console.log("I'm test module");
-};
-
-test();
->>>>>>> 011109390163c64b663975af5db084600e6a1cc6
